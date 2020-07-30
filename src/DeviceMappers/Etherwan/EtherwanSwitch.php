682a5d73a5d81b2da6495ea47761373009f1327c
Simon Westlake <simon@sonar.software>
2020-07-30 16:59:42 -0500

    MikroTik & Etherwan

--- src/DeviceMappers/Etherwan/EtherwanSwitch.php
+++ src/DeviceMappers/Etherwan/EtherwanSwitch.php
@@ -2,9 +2,54 @@
 
 namespace Poller\DeviceMappers\Etherwan;
 
+use Exception;
 use Poller\DeviceMappers\BaseDeviceMapper;
+use Poller\Models\SnmpResult;
+use Poller\Services\Formatter;
 
 class EtherwanSwitch extends BaseDeviceMapper
 {
 
+    public function map(SnmpResult $snmpResult)
+    {
+        return $this->getBridgingTable(parent::map($snmpResult));
+    }
+
+    /**
+     * Get the bridging table
+     * @param SnmpResult $snmpResult
+     * @return SnmpResult
+     */
+    private function getBridgingTable(SnmpResult $snmpResult):SnmpResult
+    {
+        $switchingDatabasePortNumbers = $this->walk(".1.3.6.1.4.1.2736.1.1.11.1.1.4");
+        $mapping = [];
+        foreach ($switchingDatabasePortNumbers as $oid)
+        {
+            $boom = explode(".", $oid->getOid());
+            $mapping[$boom[count($boom)-1]] = $oid->getValue()->__toString();
+        }
+
+        $macAddresses = $this->walk("1.3.6.1.4.1.2736.1.1.11.1.1.2");
+        foreach ($macAddresses as $oid)
+        {
+            $macAddress = Formatter::formatMac($oid->getValue()->__toString());
+            $boom = explode(".",$oid->getOid());
+            if (isset($mapping[$boom[count($boom)-1]]))
+            {
+                try {
+                    $existingMacs = $this->interfaces[$mapping[$boom[count($boom)-1]]]->getConnectedLayer2Macs();
+                    $existingMacs[] = $macAddress;
+                    $this->interfaces[$mapping[$boom[count($boom)-1]]]->setConnectedLayer2Macs($macAddresses);
+                }
+                catch (Exception $e)
+                {
+                    //
+                }
+            }
+        }
+
+        $snmpResult->setInterfaces($this->interfaces);
+        return $snmpResult;
+    }
 }
--- src/DeviceMappers/MikroTik/MikroTik.php
+++ src/DeviceMappers/MikroTik/MikroTik.php
@@ -2,9 +2,51 @@
 
 namespace Poller\DeviceMappers\MikroTik;
 
+use Exception;
 use Poller\DeviceMappers\BaseDeviceMapper;
+use Poller\Models\SnmpResult;
+use Poller\Services\Formatter;
 
 class MikroTik extends BaseDeviceMapper
 {
+    public function map(SnmpResult $snmpResult)
+    {
+        return $this->getWirelessClients(parent::map($snmpResult));
+    }
 
+    /**
+     * @param SnmpResult $snmpResult
+     * @return array|mixed
+     */
+    private function getWirelessClients(SnmpResult $snmpResult):SnmpResult
+    {
+        try {
+            $result = $this->walk("1.3.6.1.4.1.14988.1.1.1.2.1.1");
+            foreach ($result as $datum)
+            {
+                $boom = explode(".", $datum->getOid());
+                $interfaceIndex = $boom[count($boom)-1];
+                try {
+                    $mac = Formatter::formatMac($datum->getValue()->__toString());
+
+                    if(isset($this->interfaces[$interfaceIndex])) {
+                        $existingMacs = $this->interfaces[$interfaceIndex]->setConnectedLayer1Macs();
+                        $existingMacs[] = $mac;
+                        $this->interfaces[$interfaceIndex]->setConnectedLayer1Macs($existingMacs);
+                    }
+                }
+                catch (Exception $e)
+                {
+                    continue;
+                }
+            }
+        }
+        catch (Exception $e)
+        {
+            //
+        }
+
+        $snmpResult->setInterfaces($this->interfaces);
+        return $snmpResult;
+    }
 }
