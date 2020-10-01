a2d6240a185d06c8cf50d058b5a985d39d4060ad
Simon Westlake <simon@sonar.software>
2020-10-01 11:35:05 -0500

    Fix a few issues with the setup script

--- setup.sh
+++ setup.sh
@@ -13,28 +13,27 @@ apt-get -y update
 apt-get install -y php7.4-cli php7.4-common php7.4-json php7.4-gmp php7.4-dev php7.4-sqlite3 php7.4-zip php7.4-fpm composer openssl git php-pear snmp
 pecl channel-update pecl.php.net
 pecl install ev
-echo "extension=ev.so" >> /etc/php/7.4/cli/php.ini
+grep -qxF 'extension=ev.so' /etc/php/7.4/cli/php.ini || echo "extension=ev.so" >> /etc/php/7.4/cli/php.ini
 
 ## Install the latest fping
 apt-get install -y build-essential
 wget https://fping.org/dist/fping-5.0.tar.gz
 tar -zxvf fping-5.0.tar.gz
-cd fping-5.0
-./configure
-make && make install
+(cd fping-5.0; ./configure; make && make install;)
 setcap cap_net_raw+ep /usr/local/sbin/fping
 
 ## Setup sysctl for better monitoring performance
-echo "net.core.somaxconn=4096" >> /etc/sysctl.conf
-echo "net.ipv4.icmp_ratelimit=0" >> /etc/sysctl.conf
-echo "net.ipv4.icmp_msgs_per_sec=100000" >> /etc/sysctl.conf
-echo "net.ipv4.icmp_msgs_burst=5000" >> /etc/sysctl.conf
-echo "fs.file-max = 500000" >> /etc/sysctl.conf
+grep -qxF 'net.core.somaxconn=4096' /etc/sysctl.conf || echo "net.core.somaxconn=4096" >> /etc/sysctl.conf
+grep -qxF 'net.ipv4.icmp_ratelimit=0' /etc/sysctl.conf || echo "net.ipv4.icmp_ratelimit=0" >> /etc/sysctl.conf
+grep -qxF 'net.ipv4.icmp_msgs_per_sec=100000' /etc/sysctl.conf || echo "net.ipv4.icmp_msgs_per_sec=100000" >> /etc/sysctl.conf
+grep -qxF 'net.ipv4.icmp_msgs_burst=5000' /etc/sysctl.conf || echo "net.ipv4.icmp_msgs_burst=5000" >> /etc/sysctl.conf
+grep -qxF 'fs.file-max = 500000' /etc/sysctl.conf || echo "fs.file-max = 500000" >> /etc/sysctl.conf
+
 /sbin/sysctl -p
-echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
-echo "DefaultLimitNOFILE=65535" >> /etc/systemd/user.conf
-echo "* soft nofile 65535" >> /etc/security/limits.conf
-echo "* hard nofile 65535" >> /etc/security/limits.conf
+grep -qxF 'DefaultLimitNOFILE=65535' /etc/systemd/system.conf || echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
+grep -qxF 'DefaultLimitNOFILE=65535' /etc/systemd/system.conf || echo "DefaultLimitNOFILE=65535" >> /etc/systemd/system.conf
+grep -qxF '* soft nofile 65535' /etc/systemd/system.conf || echo "* soft nofile 65535" >> /etc/systemd/system.conf
+grep -qxF '* hard nofile 65535' /etc/systemd/system.conf || echo "* hard nofile 65535" >> /etc/systemd/system.conf
 
 ## Clone the repo and run initial setup
 (cd /usr/share; rm -rf sonar_poller; mkdir sonar_poller; cd sonar_poller; git clone https://github.com/SonarSoftwareInc/poller.git .;)
@@ -51,6 +50,8 @@ cp /usr/share/sonar_poller/ssl/self-signed.conf /etc/nginx/snippets/
 cp /usr/share/sonar_poller/ssl/default /etc/nginx/sites-available/
 systemctl restart nginx
 
+mkdir /var/www/.composer
+chown www-data:www-data /var/www/.composer
 (cd /usr/share/sonar_poller; sudo -u www-data composer install;)
 
 ## Setup log rotation
@@ -62,7 +63,7 @@ echo "0 0 * * * root bash /usr/share/sonar_poller/upgrade.sh" > /etc/cron.d/sona
 
 ## Setup supervisor for poller
 apt-get install -y supervisor
-cp config/sonar_poller.conf /etc/supervisor/conf.d/
+cp /usr/share/sonar_poller/config/sonar_poller.conf /etc/supervisor/conf.d/
 systemctl restart supervisor
 
 ## Reboot to apply ulimit changes
