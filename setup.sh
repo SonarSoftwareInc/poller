0f35ca03441c2036eb15418df154de1addf4b9d9
andrew <andy@sonar.software>
2021-01-21 10:33:46 -0800

    Updated setup.sh and README.md to address ev compile issue, add required composer extension

--- setup.sh
+++ setup.sh
@@ -10,7 +10,12 @@ echo "Installing the Sonar poller...";
 ## Add PHP repository, setup PHP
 add-apt-repository -y ppa:ondrej/php
 apt-get -y update
-apt-get install -y php7.4-cli php7.4-common php7.4-json php7.4-gmp php7.4-dev php7.4-sqlite3 php7.4-zip php7.4-fpm composer openssl git php-pear snmp
+apt-get install -y php7.4-cli php7.4-xml php7.4-common php7.4-json php7.4-gmp php7.4-dev php7.4-sqlite3 php7.4-zip php7.4-fpm php7.4-mbstring composer openssl git php-pear snmp
+
+update-alternatives --set php /usr/bin/php7.4
+update-alternatives --set phpize /usr/bin/phpize7.4
+update-alternatives --set php-config /usr/bin/php-config7.4
+
 pecl channel-update pecl.php.net
 pecl install ev
 grep -qxF 'extension=ev.so' /etc/php/7.4/cli/php.ini || echo "extension=ev.so" >> /etc/php/7.4/cli/php.ini
@@ -65,6 +70,12 @@ echo "0 0 * * * root bash /usr/share/sonar_poller/upgrade.sh" > /etc/cron.d/sona
 apt-get install -y supervisor
 cp /usr/share/sonar_poller/config/sonar_poller.conf /etc/supervisor/conf.d/
 systemctl restart supervisor
+
+
+## Install vendor libraries needed for poller
+cd /usr/share/sonar_poller/
+composer install
+
 
 ## Reboot to apply ulimit changes
 echo "Rebooting...";
