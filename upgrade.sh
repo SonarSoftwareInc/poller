25d713b71fe137451f8e1eee2b7a2dabdf1bc216
Simon Westlake <simon@sonar.software>
2021-05-13 10:33:56 -0500

    Run composer on upgrade

--- upgrade.sh
+++ upgrade.sh
@@ -1,10 +1,15 @@
 (cd /usr/share/sonar_poller; git reset --hard origin/master; git pull;)
 
+(cd /usr/share/sonar_poller; git describe --tags > version;)
+(cd /usr/share/sonar_poller; composer install;)
+
 chown -R www-data:www-data /usr/share/sonar_poller/www
 chown -R www-data:www-data /usr/share/sonar_poller/ssl
 chown -R www-data:www-data /usr/share/sonar_poller/logs
 chown -R www-data:www-data /usr/share/sonar_poller/permanent_config
+chown -R www-data:www-data /usr/share/sonar_poller/vendor
 
-(cd /usr/share/sonar_poller; git describe --tags > version;)
+chmod +x /usr/share/sonar_poller/upgrade.sh
+
 supervisorctl update
 supervisorctl restart sonar_poller
