c32ef73bb672b9a4e43b7baee4c2d760fe16a01e
LarrySonar <larry.weidig@sonar.software>
2025-03-20 19:25:46 -0500

    Step 1 - New upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,7 +1,22 @@
-(cd /usr/share/sonar_poller; git reset --hard origin/master; git pull;)
+#!/bin/bash
 
-(cd /usr/share/sonar_poller; git describe --tags > version;)
-(cd /usr/share/sonar_poller; composer install;)
+GIT_BRANCH="origin/master" # Default branch
+
+# Detect Ubuntu version
+UBUNTU_VERSION=$(lsb_release -rs)
+if [[ "$UBUNTU_VERSION" == "20.04" ]]; then
+  echo "Currently running maintenance release for Ubuntu 20.04, please consider upgrading to Ubuntu 24.04 for updates!"
+  GIT_BRANCH="origin/ubuntu20_04"
+fi
+
+# Switch to the appropriate branch and pull updates
+cd /usr/share/sonar_poller
+git reset --hard "$GIT_BRANCH"
+git pull "$GIT_BRANCH"
+
+# Update version and dependencies
+git describe --tags > version
+sudo -u www-data composer install
 
 chown -R www-data:www-data /usr/share/sonar_poller/www
 chown -R www-data:www-data /usr/share/sonar_poller/ssl
